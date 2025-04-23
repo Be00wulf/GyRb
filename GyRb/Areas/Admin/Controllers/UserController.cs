d44120ae618ed83a646fd85008870ff1eb54e769
Lic.Cantinas <majojedi@gmail.com>
2025-04-22 20:39:45 -0600

    acesos de usuario y admin probados y agregada la pagina de accesos denegados

--- GyRb/Areas/Admin/Controllers/UserController.cs
+++ GyRb/Areas/Admin/Controllers/UserController.cs
@@ -159,7 +159,7 @@ namespace GyRb.Areas.Admin.Controllers
             {
                 return View(new LoginVM());
             }
-            return RedirectToAction("Index", "User", new {area="Admin"});
+            return RedirectToAction("Index", "Post", new {area="Admin"});
         }
 
         [HttpPost("Login")]
@@ -186,16 +186,25 @@ namespace GyRb.Areas.Admin.Controllers
             }
             await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
             _notification.Success("Login successful");
-            return RedirectToAction("Index", "User", new { area = "Admin" });
+            return RedirectToAction("Index", "Post", new { area = "Admin" });
         }
 
         [HttpPost]
+        [Authorize]
         public IActionResult Logout()
         {
             _signInManager.SignOutAsync();
             _notification.Success("You are logged out successfully");
             return RedirectToAction("Index", "Home", new {area = ""});
         }
+
+        [HttpGet("AccessDenied")]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+
 
     }
 }
