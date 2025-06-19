84fbbee7a219b2e44c3bb824760687bba7efa034
Lic.Cantinas <majojedi@gmail.com>
2025-06-19 14:54:31 -0600

    modulos contacto, conocenos y sobre nosotros, agregados

---
+++ GyRb/Areas/Admin/Controllers/PageController.cs
@@ -0,0 +1,183 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using GyRb.Data;
+using GyRb.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GyRb.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class PageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PageController(ApplicationDbContext context, INotyfService notification, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _notification = notification;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        //about
+        [HttpGet]
+        public async Task<IActionResult> About()
+        {
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == "about");
+            var vm = new PageVM()
+            {
+                Id = page!.Id,
+                Title = page.Title,
+                ShortDescription = page.ShortDescription,
+                Description = page.Description,
+                ThumbnailUrl = page.ThumbnailUrl
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> About(PageVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == "about");
+            if (page == null)
+            {
+                _notification.Error("Page not found");
+                return View();
+            }
+
+            page.Title = vm.Title;
+            page.ShortDescription = vm.ShortDescription;
+            page.Description = vm.Description;
+
+            if (vm.Thumbnail != null)
+            {
+                page.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.SaveChangesAsync();
+            _notification.Success("About page updated successfully.");
+            return RedirectToAction("About", "Page", new { area = "Admin"});
+        }
+
+        
+
+        //contact
+        [HttpGet]
+        public async Task<IActionResult> Contact()
+        {
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == "contact");
+            var vm = new PageVM()
+            {
+                Id = page!.Id,
+                Title = page.Title,
+                ShortDescription = page.ShortDescription,
+                Description = page.Description,
+                ThumbnailUrl = page.ThumbnailUrl
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(PageVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == "contact");
+            if (page == null)
+            {
+                _notification.Error("Page not found");
+                return View();
+            }
+
+            page.Title = vm.Title;
+            page.ShortDescription = vm.ShortDescription;
+            page.Description = vm.Description;
+
+            if (vm.Thumbnail != null)
+            {
+                page.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.SaveChangesAsync();
+            _notification.Success("Contact page updated successfully.");
+            return RedirectToAction("Contact", "Page", new { area = "Admin" });
+        }
+
+        //privcy policy
+        [HttpGet]
+        public async Task<IActionResult> Privacy()
+        {
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == "privacy");
+            var vm = new PageVM()
+            {
+                Id = page!.Id,
+                Title = page.Title,
+                ShortDescription = page.ShortDescription,
+                Description = page.Description,
+                ThumbnailUrl = page.ThumbnailUrl
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Privacy(PageVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == "privacy");
+            if (page == null)
+            {
+                _notification.Error("Page not found");
+                return View();
+            }
+
+            page.Title = vm.Title;
+            page.ShortDescription = vm.ShortDescription;
+            page.Description = vm.Description;
+
+            if (vm.Thumbnail != null)
+            {
+                page.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.SaveChangesAsync();
+            _notification.Success("Privacy page updated successfully.");
+            return RedirectToAction("Privacy", "Page", new { area = "Admin" });
+        }
+
+
+        //upload image
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filepath = Path.Combine(folderPath, uniqueFileName);
+
+            using (FileStream fileStream = System.IO.File.Create(filepath))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return uniqueFileName;
+        }
+
+
+    }
+}
--- GyRb/Migrations/20250619053007_thumbnailAddedOnPage.Designer.cs
+++ GyRb/Migrations/20250619053007_thumbnailAddedOnPage.Designer.cs
@@ -4,6 +4,7 @@ using GyRb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GyRb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250619053007_thumbnailAddedOnPage")]
+    partial class thumbnailAddedOnPage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
