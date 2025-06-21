3d91e6de617ee9e11cee1ddd93dd0e3dd4610bbd
Lic.Cantinas <majojedi@gmail.com>
2025-06-20 18:08:23 -0600

    setting module agregado

--- GyRb/Areas/Admin/Controllers/SettingController.cs
+++ GyRb/Areas/Admin/Controllers/SettingController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using GyRb.Data;
+using GyRb.Models;
+using GyRb.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GyRb.Areas.Admin.Controllers
 {
@@ -8,9 +15,107 @@ namespace GyRb.Areas.Admin.Controllers
 
     public class SettingController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public SettingController(ApplicationDbContext context, 
+            INotyfService notyfService, IWebHostEnvironment webHostEnvironment)
         {
-            return View();
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var settings = _context.Settings!.ToList();
+            if(settings.Count > 0)
+            {
+                var vm = new SettingVM()
+                {
+                    Id = settings[0].Id,
+                    SiteName = settings[0].SiteName,
+                    Title = settings[0].Title,
+                    ShortDescription = settings[0].ShortDescription,
+                    ThumbnailUrl = settings[0].ThumbnailUrl,
+                    FacebookUrl = settings[0].FacebookUrl,
+                    GithubUrl = settings[0].GithubUrl,
+                    TwitterUrl = settings[0].TwitterUrl
+                    
+                };
+                return View(vm);
+            }
+
+            var setting = new Setting()
+            {
+                SiteName = "Demo Name"
+            };
+
+            await _context.Settings!.AddAsync(setting);
+            await _context.SaveChangesAsync();
+
+            var createdSettings = _context.Settings!.ToList();
+            var createdVM = new SettingVM()
+            {
+                Id = createdSettings[0].Id,
+                SiteName = createdSettings[0].SiteName,
+                Title = createdSettings[0].Title,
+                ShortDescription = createdSettings[0].ShortDescription,
+                ThumbnailUrl = createdSettings[0].ThumbnailUrl,
+                FacebookUrl = createdSettings[0].FacebookUrl,
+                GithubUrl = createdSettings[0].GithubUrl,
+                TwitterUrl = createdSettings[0].TwitterUrl
+            };
+
+            return View(createdVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SettingVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var setting = await _context.Settings!.FirstOrDefaultAsync(x => x.Id == vm.Id);
+            if (setting == null)
+            {
+                _notification.Error("...nope, nothing found.");
+                return View(vm);
+            }
+            setting.SiteName = vm.SiteName;
+            setting.Title = vm.Title;
+            setting.ShortDescription = vm.ShortDescription;
+            setting.FacebookUrl = vm.FacebookUrl;
+            setting.GithubUrl = vm.GithubUrl;
+            setting.TwitterUrl = vm.TwitterUrl;
+
+            if(vm.Thumbnail != null)
+            {
+                setting.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+            await _context.SaveChangesAsync();
+            _notification.Success("Settings updated successfully.");
+            return RedirectToAction("Index", "Setting", new {area = "Admin"} );
+        }
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filepath = Path.Combine(folderPath, uniqueFileName);
+
+            using (FileStream fileStream = System.IO.File.Create(filepath))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return uniqueFileName;
+        }
+
+
     }
 }
