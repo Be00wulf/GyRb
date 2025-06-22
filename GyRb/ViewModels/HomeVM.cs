970af063bda907c6edccb7e5231e0677615f85ee
Lic.Cantinas <majojedi@gmail.com>
2025-06-21 18:41:40 -0600

    editando diseño de home page

---
+++ GyRb/ViewModels/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using GyRb.Models;
+
+namespace GyRb.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post> Posts { get; set; } 
+
+
+
+    }
+}
