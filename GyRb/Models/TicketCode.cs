7a214d597d08463014cc06bb32911eb33c92a912
Lic.Cantinas <majojedi@gmail.com>
2025-08-06 17:53:14 -0600

    starting ticket module in a new branch

---
+++ GyRb/Models/TicketCode.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GyRb.Models
+{
+    public class TicketCode
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Code { get; set; } = string.Empty;
+
+        public DateTime? UsedAt { get; set; }
+        
+        public bool IsUsed => UsedAt.HasValue;
+    }
+}
